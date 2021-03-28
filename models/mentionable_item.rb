ce7cc1933ba24d9aa992301b300a0749f7dbc22a
Robert Barrow <merefield@gmail.com>
2021-03-28 08:45:57 +0100

    prevent records with invalid urls being added

--- models/mentionable_item.rb
+++ models/mentionable_item.rb
@@ -4,22 +4,27 @@ class MentionableItem < ActiveRecord::Base
 
   def self.add!(mentionable_item)
 
-    if !mentionable_item.has_key?(:image_url) || mentionable_item.has_key?(:image_url) && mentionable_item[:image_url].blank?
-      document = Nokogiri::HTML(Oneboxer.preview(mentionable_item[:url]))
-      mentionable_item[:image_url] = document.css('.thumbnail').attr('src').value
-    end
+    unless (!mentionable_item.has_key?(:url) || mentionable_item[:url] =~ URI::regexp)
 
-    unless !MentionableItem.find_by(url: mentionable_item[:url]).nil?
-      self.create!(
-        url: mentionable_item[:url],
-        name: mentionable_item[:name],
-        image_url: mentionable_item[:image_url],
-        description:mentionable_item[:description],
-        created_at:  Time.zone.now,
-        updated_at: Time.zone.now,
-      )
+      if !mentionable_item.has_key?(:image_url) || mentionable_item.has_key?(:image_url) && mentionable_item[:image_url].blank?
+        document = Nokogiri::HTML(Oneboxer.preview(mentionable_item[:url]))
+        mentionable_item[:image_url] = document.css('.thumbnail').attr('src').value
+      end
+
+      unless !MentionableItem.find_by(url: mentionable_item[:url]).nil?
+        self.create!(
+          url: mentionable_item[:url],
+          name: mentionable_item[:name],
+          image_url: mentionable_item[:image_url],
+          description:mentionable_item[:description],
+          created_at:  Time.zone.now,
+          updated_at: Time.zone.now,
+        )
+      else
+        puts I18n.t('sheet_ingest.warnings.duplicate_url')
+      end
     else
-      puts I18n.t('sheet_ingest.warnings.duplicate_url')
+      puts I18n.t('sheet_ingest.warnings.no_valid_url')
     end
   end
 
