923052102405c9b17ebdc206880a42a866a24777
Robert Barrow <merefield@gmail.com>
2021-03-28 19:08:40 +0100

    uncommented out commented out working test

--- spec/lib/worksheets_import_spec.rb
+++ spec/lib/worksheets_import_spec.rb
@@ -6,10 +6,10 @@ describe ::MentionableItems::WorksheetsImport do
   worsheet_with_url = [["url",""], ["https:\\news.bbc.co.uk",""]]
   worsheet_with_url_and_name = [["url","", "name"], ["https:\\linkin.com","","Meghan"], ["https:\\news.bbc.co.uk","","Harry"]]
 
-  # it "Importing an empty sheet results in no records" do
-  #   result = ::MentionableItems::WorksheetsImport.import_sheet(empty_worksheet)
-  #   expect(result[:success_rows]).to eq(0)
-  # end
+  it "Importing an empty sheet results in no records" do
+    result = ::MentionableItems::WorksheetsImport.import_sheet(empty_worksheet)
+    expect(result[:success_rows]).to eq(0)
+  end
 
   it "Importing a sheet with a valid url results in one successful import" do
     result = ::MentionableItems::WorksheetsImport.import_sheet(worsheet_with_url)
