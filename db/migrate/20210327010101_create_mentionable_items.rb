aff7a5494097ab505882cd7f51b5940fa1ba9308
Robert Barrow <merefield@gmail.com>
2021-03-28 08:14:05 +0100

    Add unique index to table to prevent duplicates

--- db/migrate/20210327010101_create_mentionable_items.rb
+++ db/migrate/20210327010101_create_mentionable_items.rb
@@ -3,13 +3,15 @@
 class CreateMentionableItems < ActiveRecord::Migration[5.2]
   def up
     create_table :mentionable_items do |t|
-      t.string :url, null: false, unique: true
+      t.string :url, null: false
       t.string :name
       t.string :image_url
       t.string :description
       t.datetime :created_at
       t.datetime :updated_at
     end
+
+    add_index :mentionable_items, :url, unique: true
   end
 
   def down
