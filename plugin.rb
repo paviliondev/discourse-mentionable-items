1d17e2487b852c225272d6813b8f0d7f0db83e68
angusmcleod <angus@mcleod.org.au>
2021-04-22 14:51:24 +1000

    IMPROVE: update gem list
    These gems are in discourse/gemfile.lock: rexml, openssl. Also updated the format slightly and added gems to gitignore

--- plugin.rb
+++ plugin.rb
@@ -5,28 +5,26 @@
 # authors: Robert Barrow
 # url: https://github.com/paviliondev/discourse-mentionable-items
 
-gem 'gems', '1.2.0', { require: false }
-gem 'webrick', '1.7.0', { require: false }
-gem 'rexml', '3.2.4', { require: false }
-gem 'httpclient', '2.8.3', { require: false }
-gem 'retriable', '3.1.2', { require: false }
-gem 'signet', '0.15.0', { require: false }
-gem 'os', '1.1.1', { require: false }
-gem 'memoist', '0.16.2', { require: false }
-gem 'declarative-option', '0.1.0', { require: false }
-gem 'declarative', '0.0.20', { require: false }
-gem 'trailblazer-option', '0.1.0', { require: false }
-gem 'uber','0.1.0', { require: false }
-gem 'representable', '3.0.4', { require: false }
-gem 'googleauth', '0.16.0', { require: false }
-gem 'google-apis-core', '0.3.0', { require: false }
-gem 'google-apis-discovery_v1', '0.2.0', { require: false }
-gem 'google-apis-generator', '0.2.0', { require: false }
-gem 'google-api-client', '0.53.0', { require: false }
-gem 'google_drive', '3.0.6', { require: false }
-gem 'openssl', '2.2.0', { require: false }
-gem 'base64url', '1.0.1', { require: false }
-gem 'friendly_id', '5.4.2', {require: false }
+gem 'gems', '1.2.0', require: false
+gem 'webrick', '1.7.0', require: false
+gem 'httpclient', '2.8.3', require: false
+gem 'retriable', '3.1.2', require: false
+gem 'signet', '0.15.0', require: false
+gem 'os', '1.1.1', require: false
+gem 'memoist', '0.16.2', require: false
+gem 'declarative-option', '0.1.0', require: false
+gem 'declarative', '0.0.20', require: false
+gem 'trailblazer-option', '0.1.0', require: false
+gem 'uber','0.1.0', require: false
+gem 'representable', '3.0.4', require: false
+gem 'googleauth', '0.16.0', require: false
+gem 'google-apis-core', '0.3.0', require: false
+gem 'google-apis-discovery_v1', '0.2.0', require: false
+gem 'google-apis-generator', '0.2.0', require: false
+gem 'google-api-client', '0.53.0', require: false
+gem 'google_drive', '3.0.6', require: false
+gem 'base64url', '1.0.1', require: false
+gem 'friendly_id', '5.4.2', require: false
 
 enabled_site_setting :mentionable_items_enabled
 
