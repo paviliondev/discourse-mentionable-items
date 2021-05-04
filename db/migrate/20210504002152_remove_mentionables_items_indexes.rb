e28333853d027eb339cd013e8ccc63c5740c8be8
angusmcleod <angus@mcleod.org.au>
2021-05-05 07:55:26 +1200

    FIX: incorrect migration name

--- db/migrate/20210504002152_remove_mentionables_items_indexes.rb
+++ db/migrate/20210504002152_remove_mentionables_items_indexes.rb
@@ -1,4 +1,4 @@
-class MentionablesSlugDataConsolidation < ActiveRecord::Migration[6.0]
+class RemoveMentionablesItemsIndexes < ActiveRecord::Migration[6.0]
   def change
     remove_index :mentionable_items, :name_slug
     remove_index :mentionable_items, :url
