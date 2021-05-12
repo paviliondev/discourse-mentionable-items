70ad492f6086f8198ee9b9b2833e35d4f613a991
angusmcleod <angus@mcleod.org.au>
2021-05-13 08:06:30 +1200

    Make import job a normal job

--- jobs/import_mentionable_items.rb
+++ jobs/import_mentionable_items.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 module Jobs
-  class ImportMentionableItems < ::Jobs::Scheduled
-    every 24.hours
-
+  class ImportMentionableItems < ::Jobs::Base
     def execute(args={})
       source_name = SiteSetting.mentionable_items_source.to_s
       klass = "MentionableItems::#{source_name.camelize}".constantize
