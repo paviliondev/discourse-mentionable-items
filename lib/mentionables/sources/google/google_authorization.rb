49c99727d7b49074cdc210e4b7c8a71d3d1884fe
merefield <merefield@gmail.com>
2022-12-01 21:01:49 +0000

    clean up code

--- lib/mentionables/sources/google/google_authorization.rb
+++ lib/mentionables/sources/google/google_authorization.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
-# require 'excon'
-# require 'jwt'
-# require "base64url"
 
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
@@ -15,57 +12,6 @@ class Mentionables::GoogleAuthorization
   )
   BASE_API_URL = 'https://accounts.google.com/o/oauth2/auth'
   TOKEN_URL = 'https://oauth2.googleapis.com/token'
-  # GRANT_TYPE = "urn:ietf:params:oauth:grant-type:jwt-bearer"
-
-  # def self.access_token
-  #   request_access_token if !authorized
-  #   get_access_token[:token]
-  # end
-
-  # def self.set_access_token(data)
-  #   PluginStore.set(Mentionables::PLUGIN_NAME, 'google_sheets_access_token', data)
-  # end
-
-  # def self.get_access_token
-  #   PluginStore.get(Mentionables::PLUGIN_NAME, 'google_sheets_access_token') || {}
-  # end
-
-  # def self.calculate_jwt
-  #   header = { "alg": "RS256", "typ": "JWT" }
-  #   headerJWT = Base64URL.encode(JSON.generate(header))
-  #   claims = {
-  #     "iss": SiteSetting.mentionables_google_service_account_email,
-  #     "scope": SCOPES.join(" "),
-  #     "aud": TOKEN_URL,
-  #     "exp": Time.now.to_i + 3600,
-  #     "iat": Time.now.to_i,
-  #   }
-  #   claimsJWT = Base64URL.encode(JSON.generate(claims))
-  #   private_key = SiteSetting.mentionables_google_service_account_private_key
-  #   rsa_public = OpenSSL::PKey::RSA.new(private_key.gsub("\\n", "\n"))
-  #   sig = JWT::Signature.sign('RS256', "#{headerJWT}.#{claimsJWT}", rsa_public)
-  #   sig64 = Base64URL.encode(sig)
-
-  #   "#{headerJWT}.#{claimsJWT}.#{sig64}"
-  # end
-
-  # def self.request_access_token
-  #   # token = calculate_jwt
-  #   # body = {
-  #   #   grant_type: GRANT_TYPE,
-  #   #   assertion: token
-  #   # }
-
-  #   # result = Excon.post("#{BASE_API_URL}/token",
-  #   #   headers: {
-  #   #     "Content-Type" => "application/x-www-form-urlencoded"
-  #   #   },
-  #   #   body: URI.encode_www_form(body)
-  #   # )
-
-  #   # handle_token_result(result)
-
-  # end
 
   def self.authorizer
     authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
@@ -82,40 +28,4 @@ class Mentionables::GoogleAuthorization
       token_uri: BASE_API_URL,
     }.to_json
   end
-
-  # @credentials ||= {
-  #   type: "service_account",
-  #   project_id: Rails.application.credentials.google[:project_id],
-  #   private_key_id: Rails.application.credentials.google[:private_key_id],
-  #   private_key: Rails.application.credentials.google[:private_key],
-  #   client_email: Rails.application.credentials.google[:client_email],
-  #   client_id: Rails.application.credentials.google[:client_id],
-  #   auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  #   token_uri: "https://oauth2.googleapis.com/token",
-  #   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  #   client_x509_cert_url: Rails.application.credentials.google[:client_x509_cert_url],
-  # }.to_json
-
-  # def self.handle_token_result(result)
-  #   data = JSON.parse(result.body)
-
-  #   return false if (data['error'])
-
-  #   token = data['access_token']
-  #   expires_at = Time.now + data['expires_in'].seconds
-  #   refresh_at = expires_at.to_time - 1.minutes
-
-  #   Jobs.enqueue_at(refresh_at, :refresh_google_access_token)
-
-  #   set_access_token(
-  #     token: token,
-  #     expires_at: expires_at,
-  #     refresh_at: refresh_at
-  #   )
-  # end
-
-  # def self.authorized
-  #   stored = get_access_token
-  #   stored[:token] && stored[:expires_at].to_datetime > Time.now
-  # end
 end
--- lib/mentionables/sources/google/google_sheets.rb
+++ lib/mentionables/sources/google/google_sheets.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-#require 'google_drive'
 require "google/apis/sheets_v4"
 
 class ::Mentionables::GoogleSheets < ::Mentionables::Source
@@ -9,23 +8,14 @@ class ::Mentionables::GoogleSheets < ::Mentionables::Source
     super
     spreadsheet_result = spreadsheet.present? ? spreadsheet : request_spreadsheet
 
-    # if spreadsheet_result&.class == ::GoogleDrive::Spreadsheet
     @spreadsheet = spreadsheet_result
     @ready = true
-    # else
-    #   if spreadsheet_result.is_a?(Hash) && spreadsheet_result[:error_key]
-    #     message = I18n.t("mentionables.google_sheets.#{spreadsheet_result[:error_key]}")
-    #   elsif message = spreadsheet_result.try(:message)
-    #     message = message
-    #   else
-    #     message = I18n.t("mentionables.google_sheets.failed_to_retrieve_spreadsheet")
-    #   end
-  # rescue => error
-  #   Mentionables::Log.create(
-  #     type: ::Mentionables::Log.types[:error],
-  #     source: source_name,
-  #     message: error
-  #   )
+  rescue => error
+    Mentionables::Log.create(
+      type: ::Mentionables::Log.types[:error],
+      source: source_name,
+      message: error
+    )
   end
 
   def source_name
@@ -34,13 +24,6 @@ class ::Mentionables::GoogleSheets < ::Mentionables::Source
 
   def get_items_from_source
     spreadsheet_id = SiteSetting.mentionables_google_spreadsheet_id
-    # worksheets = @spreadsheet.sheets
-
-    # client.get_spreadsheet_values(spreadsheet_id, "Sheet1!A1:D1000")
-
-    # if (gids = SiteSetting.mentionables_google_worksheet_gids.split('|')).any?
-    #   worksheets = worksheets.select { |w| gids.include?(w.gid) }
-    # end
 
     sheets = SiteSetting.mentionables_google_worksheet_names.split('|')
 
@@ -77,9 +60,6 @@ class ::Mentionables::GoogleSheets < ::Mentionables::Source
 
   def request_spreadsheet
     begin
-      # access_token = Mentionables::GoogleAuthorization.access_token
-      # return { error_key: 'failed_to_authorize' } if !access_token.present?
-
       spreadsheet_id = SiteSetting.mentionables_google_spreadsheet_id
       return { error_key: 'no_spreadsheet_id' } if spreadsheet_id.blank?
 
@@ -88,12 +68,12 @@ class ::Mentionables::GoogleSheets < ::Mentionables::Source
       client.authorization = Mentionables::GoogleAuthorization.authorizer
 
       client
-    # rescue => error
-    #   Mentionables::Log.create(
-    #     type: ::Mentionables::Log.types[:error],
-    #     source: source_name,
-    #     message: error
-    #   )
+    rescue => error
+      Mentionables::Log.create(
+        type: ::Mentionables::Log.types[:error],
+        source: source_name,
+        message: error
+      )
     end
   end
 end
