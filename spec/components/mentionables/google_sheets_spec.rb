b55421dbdd60d9d9997e426912285427ab6600c0
merefield <merefield@gmail.com>
2022-12-05 21:42:58 +0000

    some non working progress on tests

--- spec/components/mentionables/google_sheets_spec.rb
+++ spec/components/mentionables/google_sheets_spec.rb
@@ -5,48 +5,75 @@ describe ::Mentionables::GoogleSheets do
   FIXTURE_PATH = "#{Rails.root}/plugins/discourse-mentionables/spec/fixtures"
 
   before(:all) do
-    WebMock.allow_net_connect!
+    # WebMock.allow_net_connect!
     SiteSetting.mentionables_onebox_fallback = false
-    @session = GoogleDrive::Session.from_service_account_key("#{FIXTURE_PATH}/google_sheets/service-account.json")
-  end
+    # @session = GoogleDrive::Session.from_service_account_key("#{FIXTURE_PATH}/google_sheets/service-account.json")
+    # allow(Mentionables::GoogleAuthorization).to 
+    # receive(:authorizer).and_return({})
 
-  after(:all) do
-    @session.spreadsheets.each do |sheet|
-      sheet.delete(true)
+    #Mentionables::GoogleAuthorization.stubs(:authorizer).and_return({})
+    Mentionables::GoogleAuthorization.stubs(:authorizer).returns({})
+    
+    # stub.any_instance_of(Mentionables::GoogleAuthorization.authorizer).
+  end
+  context "spreadsheet has required columns" do
+    before do
+    
+      stub.any_instance_of(Google::Apis::SheetsV4::SheetsService).get_spreadsheet_values {
+        [["name", "url", "description", "affiliate_snippet_1"],
+        ["Tomato", "https://example.com/tomato", "A Tomato", "<div>Tomato</div>"],
+        ["Orange", "https://example.com/tomato", "An Orange", "<div>Orange</div>"],
+        ["Cucumber", "https://example.com/tomato", "A Cucumber", "<div>Cucumber</div>"]]
+        }
+    end
+
+    it "Importing a sheet with required columns works" do
+      workbook = ::Mentionables::GoogleSheets.new
+      workbook.request_spreadsheet
+      result = workbook.import
+      # result = described_class.new(spreadsheet).import
+  
+      expect(result.success).to eq(1)
+      expect(result.error).to eq(0)
+      expect(result.duplicate).to eq(0)
+      expect(MentionableItem.all.size).to eq(3)
     end
   end
 
-  def create_spreadsheet(filename)
-    @session.upload_from_file("#{FIXTURE_PATH}/#{filename}.csv", "Test: #{filename}")
-  end
+    # it "Importing a sheet with optional columns works" do
+    #   spreadsheet = create_spreadsheet("required_and_optional")
+    #   result = described_class.new(spreadsheet).import
+  
+    #   expect(result.success).to eq(1)
+    #   expect(result.error).to eq(0)
+    #   expect(result.duplicate).to eq(0)
+    #   expect(MentionableItem.all.size).to eq(1)
+    # end
 
-  it "Importing an empty sheet does nothing" do
-    spreadsheet = create_spreadsheet("empty")
-    result = described_class.new(spreadsheet).import
+ #   sheets.spreadsheet.get_spreadsheet_values(SiteSetting.mentionables_google_spreadsheet_id, "Sheet1!A1:H#{SiteSetting.mentionables_google_worksheet_max_row}").values
+ 
 
-    expect(result.success).to eq(0)
-    expect(result.error).to eq(0)
-    expect(result.duplicate).to eq(0)
-    expect(MentionableItem.all.size).to eq(0)
-  end
+  # end
 
-  it "Importing a sheet with required columns works" do
-    spreadsheet = create_spreadsheet("required_only")
-    result = described_class.new(spreadsheet).import
+  # after(:all) do
+  #   @session.spreadsheets.each do |sheet|
+  #     sheet.delete(true)
+  #   end
+  # end
 
-    expect(result.success).to eq(1)
-    expect(result.error).to eq(0)
-    expect(result.duplicate).to eq(0)
-    expect(MentionableItem.all.size).to eq(1)
-  end
+  # def create_spreadsheet(filename)
+  #   @session.upload_from_file("#{FIXTURE_PATH}/#{filename}.csv", "Test: #{filename}")
+  # end
 
-  it "Importing a sheet with optional columns works" do
-    spreadsheet = create_spreadsheet("required_and_optional")
-    result = described_class.new(spreadsheet).import
+  # it "Importing an empty sheet does nothing" do
+  #   spreadsheet = create_spreadsheet("empty")
+  #   result = described_class.new(spreadsheet).import
 
-    expect(result.success).to eq(1)
-    expect(result.error).to eq(0)
-    expect(result.duplicate).to eq(0)
-    expect(MentionableItem.all.size).to eq(1)
-  end
+  #   expect(result.success).to eq(0)
+  #   expect(result.error).to eq(0)
+  #   expect(result.duplicate).to eq(0)
+  #   expect(MentionableItem.all.size).to eq(0)
+  # end
+
+
 end
