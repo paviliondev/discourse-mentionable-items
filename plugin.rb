dca4c79a1001af70c356ccfc4bea075b0ec97cc3
merefield <merefield@gmail.com>
2024-10-06 22:15:44 +0100

    update dependency

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 # name: discourse-mentionables
 # about: Allows users to +mention custom data in posts
-# email contacts: robert@thepavilion.io
-# version: 0.5.1
+# email contacts: robert@pavilion.tech
+# version: 0.5.2
 # authors: Robert Barrow, Angus McLeod
 # contact_emails: development@pavilion.tech
 # url: https://github.com/paviliondev/discourse-mentionables
 
-gem 'webrick', '1.8.1', require: false
+gem 'webrick', '1.8.2', require: false
 gem 'httpclient', '2.8.3', require: false
 gem 'retriable', '3.1.2', require: false
 gem 'signet', '0.17.0', require: false
